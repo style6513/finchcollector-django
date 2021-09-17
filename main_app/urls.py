fc2fa90b0b90fdc80e7247bb8ca44523aa69950e
DaeyoungH <style6513@gmail.com>
2021-09-17 15:01:49 -0700

    before AWS & adding photo upload func

--- main_app/urls.py
+++ main_app/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path('toys/', views.ToyList.as_view(), name='toys_list'),
     path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
     path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
-    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_update'),
     path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
     path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
     path('finches/<int:finch_id>/assoc_toy/<int:toy_id>', views.assoc_toy, name='assoc_toy'),
