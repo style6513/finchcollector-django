6f6e0ffb83cbb0a2a0ae685ce3fb832deedc6b6b
DaeyoungH <style6513@gmail.com>
2021-09-17 13:58:00 -0700

    day5-set up toy&cats association

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,8 +11,10 @@ urlpatterns = [
     path('finches/<int:pk>/delete/', views.FinchDelete.as_view(), name='finch_delete'),
     path('finches/<int:finch_id>/add_feeding/', views.add_feeding, name='add_feeding'),
     path('toys/', views.ToyList.as_view(), name='toys_list'),
+    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
     path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
     path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_update'),
     path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
     path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
+    path('finches/<int:finch_id>/assoc_toy/<int:toy_id>', views.assoc_toy, name='assoc_toy'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -34,16 +34,17 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
+    toys_finch_doesnt_have = Toy.objects.exclude(id__in = finch.toys.all().values_list('id'))
     feeding_form = FeedingForm()
     return render(request,'finches/detail.html',{
         'finch': finch,
-        'feeding_form': feeding_form
+        'feeding_form': feeding_form,
+        'toys': toys_finch_doesnt_have
     })
 
 class FinchCreate(CreateView):
     model = Finch
-    fields = '__all__'
-    success_url = '/finches'
+    fields = ['name', 'breed','description','age']
 
 class FinchUpdate(UpdateView):
     model = Finch
@@ -78,3 +79,7 @@ class ToyUpdate(UpdateView):
 class ToyDelete(DeleteView):
     model = Toy
     success_url = '/toys'
+
+def assoc_toy(request, finch_id, toy_id):
+    Finch.objects.get(id=finch_id).toys.add(toy_id)
+    return redirect('detail', finch_id=finch_id)
